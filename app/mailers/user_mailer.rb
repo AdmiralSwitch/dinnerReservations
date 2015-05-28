fbe6f2def804e496098ead0d32726377791708cb
Switch-Thomas <admiralswitch@gmail.com>
2015-05-28 15:53:37 -0700

    mailer didnt work, starting over

--- app/mailers/user_mailer.rb
+++
@@ -1,8 +0,0 @@
-class UserMailer < ApplicationMailer
-  default :from => "admiralswitch@gmail.com"
-
-  def registration_confirmation(guest)
-    @guest = guest
-    mail(:to => @guest.email, :subject => "Garden2Table Dinner Registration")
-  end
-end
