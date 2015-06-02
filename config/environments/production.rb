88210ee3d357019547ab8dd2f831841b920634d9
Switch-Thomas <admiralswitch@gmail.com>
2015-06-01 20:32:55 -0700

    removed mailer production code. added styling and wcc link

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,17 +76,19 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
 
-  config.action_mailer.default_url_options = { :host => 'www.sendgrid.net'}
 
-  config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = {
-    :address => "smtp.sendgrid.net",
-    :port => "587",
-    :domail => "heroku.com",
-    :authentication => :plain,
-    :enable_starttls_auto => true,
-    :user_name => ENV['SENDGRID_USERNAME'],
-    :password => ENV['SENDGRID_PASSWORD']
-  }
+  # vvv Noted out mailer for current version.vvv
+  # config.action_mailer.default_url_options = { :host => 'www.sendgrid.net'}
+
+  # config.action_mailer.delivery_method = :smtp
+  # config.action_mailer.smtp_settings = {
+  #   :address => "smtp.sendgrid.net",
+  #   :port => "587",
+  #   :domail => "heroku.com",
+  #   :authentication => :plain,
+  #   :enable_starttls_auto => true,
+  #   :user_name => ENV['SENDGRID_USERNAME'],
+  #   :password => ENV['SENDGRID_PASSWORD']
+  # }
 
 end
