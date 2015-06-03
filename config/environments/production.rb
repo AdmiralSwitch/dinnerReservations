e022c34961ecfbd2985cf0757a283f5380732c28
Switch-Thomas <admiralswitch@gmail.com>
2015-06-03 13:16:00 -0700

    production issue may have been default name in application_mailer.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,18 +78,18 @@ Rails.application.configure do
 
 
   # vvv Noted out mailer for current version.vvv
-  require 'mail'
+  config.action_mailer.default_url_options = { :host => 'www.sendgrid.net'}
 
-    Mail.defaults do
-      delivery_method :smtp, {
-        :address => 'smtp.sendgrid.net',
-        :port => '587',
-        :domain => 'heroku.com',
-        :user_name => ENV['SENDGRID_USERNAME'],
-        :password => ENV['SENDGRID_PASSWORD'],
-        :authentication => :plain,
-        :enable_starttls_auto => true
-      }
-  end
+  config.action_mailer.delivery_method = :smtp
+  ActionMailer::Base.smtp_settings = {
+  :address        => 'smtp.sendgrid.net',
+  :port           => '587',
+  :authentication => :plain,
+  :user_name      => ENV['SENDGRID_USERNAME'],
+  :password       => ENV['SENDGRID_PASSWORD'],
+  :domain         => 'heroku.com',
+  :enable_starttls_auto => true,
+  :openssl_verify_mode  => 'none'
+}
 
 end
