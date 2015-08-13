faa6d28ceb7ee40f3a08c649cedadb6ea1335694
Switch-Thomas <admiralswitch@gmail.com>
2015-08-13 16:46:30 -0700

    removed 1 line from production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,7 +78,6 @@ Rails.application.configure do
 
 
   # vvv Noted out mailer for current version.vvv
-  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'www.anything.com'}
 
 
